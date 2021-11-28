ec3594cdd6a2d1a8b9f92634d40ce0e42ba76e07
TaggsSwaggs <94643517+TaggsSwaggs@users.noreply.github.com>
2021-11-27 21:03:44 -0700

    Got the background for the menu screen working

---
+++ js/garage.js
@@ -0,0 +1,19 @@
+var garage = new Phaser.Class({
+    Extends: Phaser.Scene,
+initialize: function() {
+    Phaser.Scene.call(this, { "key": "garage" });
+},
+
+preload: function()
+{
+
+},
+
+create: function()
+{
+
+},
+update: function()
+{
+
+}});
