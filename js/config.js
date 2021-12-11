356c98168fb5f9ebda328f8669e1e764bd07d1a2
TaggsSwaggs <94643517+TaggsSwaggs@users.noreply.github.com>
2021-12-11 08:26:37 -0700

    i swear i didnt change anything i dont remember add the require function so idk what happened

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ var config = {
     parent: "DriftR",
     width: 800,
     height: 800,
-    scene: [ menu, mapselect, map2, map1, garage ],
+    scene: [ menu, mapselect, map3, map2, map1, garage ],
     physics: {
         default: 'arcade',
         arcade: {
@@ -15,3 +15,4 @@ var config = {
 
 //Defines 'DriftR' As The Game
 var DriftR = new Phaser.Game(config);
+var mainPlayer = ('mainPlayer','assets/images/Cars/Player.png');;
