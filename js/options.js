010b4c400768a04d79393cf315574413bfa634c8
Zevin Burzynski <86094612+REALziez@users.noreply.github.com>
2021-12-11 20:29:32 -0700

    added test fullscreen and options

--- js/options.js
+++ js/options.js
@@ -1,4 +1,4 @@
-var garage = new Phaser.Class({
+var options = new Phaser.Class({
     Extends: Phaser.Scene,
 initialize: function() {
     Phaser.Scene.call(this, { "key": "options" });
@@ -11,6 +11,9 @@ preload: function()
 
 create: function()
 {
+back = this.add.image(400,400, 'back')
+
+        this.scale.startFullscreen();
 
 },
 update: function()
