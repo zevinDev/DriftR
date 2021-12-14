e764438373a9315b265cb4c86465f6966f976576
Zevin Burzynski <86094612+REALziez@users.noreply.github.com>
2021-12-13 20:20:15 -0700

    test fullscreen buttons

--- js/options.js
+++ js/options.js
@@ -12,8 +12,25 @@ preload: function()
 create: function()
 {
 back = this.add.image(400,400, 'back')
+onFull = this.add.text(300, 400, 'full')
+offFull = this.add.text(400, 400, 'offFull')
+back = this.add.text(100, 100, 'back')
 
-        this.scale.startFullscreen();
+
+onFull.setInteractive();
+onFull.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+    this.scale.startFullscreen();
+})
+
+offFull.setInteractive();
+offFull.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+    this.scale.stopFullscreen();
+})
+
+back.setInteractive();
+back.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+    this.scene.start('menu')
+})
 
 },
 update: function()
