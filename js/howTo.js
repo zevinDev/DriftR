2e6b216dcfc8c7d6afc96443574557d9e475a8c1
PEREE <50774692+dri-gi@users.noreply.github.com>
2022-06-25 09:39:29 -0700

    text changes, button changes, etc
    i got to go to work man look at the code yourself

--- js/howTo.js
+++ js/howTo.js
@@ -1,144 +1,150 @@
-var howTo = new Phaser.Class({    
-    
-    
+var count = 0;
+var onCheckpoint = 0;
+var howTo = new Phaser.Class({
+
+
     Extends: Phaser.Scene,
     initialize: function() {
-        
+
         Phaser.Scene.call(this, {
             "key": "howTo"
         });
+
     },
-preload: function() {
-    gamePaused = false;
-    var car = localStorage.getItem('car');
-    this.load.image('player', car);
-    
-    
-},
+    preload: function() {
+        gamePaused = false;
+        var car = localStorage.getItem('car');
+        this.load.image('player', car);
 
-create: function(){
-    player = this.physics.add.sprite(3200, 3377, 'player');
-    player.body.setMaxSpeed(500);
-    player.angle = -90;
-    player.setBounce(0.2);
-    
 
-    camera = this.cameras.main;
-    camera.startFollow(player);
-    canMove = true;
-   
-    //code for the boostpad function
-    var usedBoostPad1 = false;
+    },
+
+    create: function() {
+        player = this.physics.add.sprite(3200, 3377, 'player');
+        player.body.setMaxSpeed(500);
+        player.angle = -90;
+        player.setBounce(0.2);
+        counter = 0;
+
+        camera = this.cameras.main;
+        camera.startFollow(player);
+        canMove = true;
+
+        firstTime = true;
+        //code for the boostpad function
+        var usedBoostPad1 = false;
         var boostpad1;
 
 
-    boostpad1 = this.physics.add.sprite(5920, 2915, 'boostPad');
-    this.anims.create({
-        key: "boost",
-        frameRate: 7,
-        frames: this.anims.generateFrameNumbers("boostPad", {
-            start: 0,
-            end: 3
-        }),
-        repeat: -1
-    });
-    boostpad1.play("boost")
-    boostpad1.setScale(.5);
-    boostpad1.angle = 180;
-    
-    //Adds a boostpad to the map
-           boostpads = this.add.group();
-           boostpads.add(boostpad1);
-   
-           cars = this.add.group();
-           cars.add(player);
-           
-   
-           //Handles BoostPads and BoostPad Collision
-           this.physics.add.overlap(cars, boostpads, function(user, boostpad) {
-               if (boostpad == boostpad1 && usedBoostPad1 == false) {
-                   usedBoostPad1 = true;
-                   boostpad1.stop("boost");
-                   boostpad1.setFrame(0);
-                   user.body.setMaxSpeed(1000);
-                   user.body.velocity.normalize()
-                       .scale(1000);
-                   setTimeout(function() {
-                       user.body.setMaxSpeed(500);
-                   }, 500);
-               }
-           });
-    
-    ranOnce = false;
-    masSpeed = 500;     
-    
-    timer = 0;
+        boostpad1 = this.physics.add.sprite(5920, 2915, 'boostPad');
+        this.anims.create({
+            key: "boost",
+            frameRate: 7,
+            frames: this.anims.generateFrameNumbers("boostPad", {
+                start: 0,
+                end: 3
+            }),
+            repeat: -1
+        });
+        boostpad1.play("boost")
+        boostpad1.setScale(.5);
+        boostpad1.angle = 180;
+
+        //Adds a boostpad to the map
+        boostpads = this.add.group();
+        boostpads.add(boostpad1);
+
+        cars = this.add.group();
+        cars.add(player);
+
+
+        //Handles BoostPads and BoostPad Collision
+        this.physics.add.overlap(cars, boostpads, function(user, boostpad) {
+            if (boostpad == boostpad1 && usedBoostPad1 == false) {
+                usedBoostPad1 = true;
+                boostpad1.stop("boost");
+                boostpad1.setFrame(0);
+                user.body.setMaxSpeed(1000);
+                user.body.velocity.normalize()
+                    .scale(1000);
+                setTimeout(function() {
+                    user.body.setMaxSpeed(500);
+                }, 500);
+            }
+        });
+
+        ranOnce = false;
+        masSpeed = 500;
+
+        timer = 0;
         timez = 0;
         seconds = 0;
         milliseconds = 0;
-    
-    tutorialText = this.add.text(200, 200, "this is placeholder text lol", {
-        fontFamily: 'Dogica',
-        fontSize: '32px'
-    });
-    tutorialText.setVisible(false); 
-tutorialText.setScrollFactor(0,0); 
 
-tutorialText2 = this.add.text(200, 200, "Do you want to engage\n in the tutorial?", {
-    fontFamily: 'Dogica',
-    fontSize: '32px'
-});
-tutorialText2.setVisible(true); 
-tutorialText2.setScrollFactor(0,0); 
+        tutorialText = this.add.text(200, 200, "Welcome to DriftR!!! \nPress the up arrow to accelerate\nand the left and right arrows to navigate!!!", {
+            fontFamily: 'Dogica',
+            fontSize: '24px'
+        });
+        tutorialText.setVisible(false);
+        tutorialText.setScrollFactor(0, 0);
 
-    var buttonz = this.add.image(255, 600, 'start');
-    buttonz.setScrollFactor(0, 0);
-   buttonz.setInteractive(); 
-    buttonz.on(pointerDown, () => {
-     this.cameras.main.fadeOut(1000, 0, 0, 0)
-     this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
-         this.scene.start('menu')
-         this.scene.stop();
-     })
- })
-    
-    var continueButtonz = this.add.image(550, 600, 'backButton'); 
-    continueButtonz.setScrollFactor(0, 0);
-    continueButtonz.setInteractive(); 
-    continueButtonz.on(pointerDown, () => {
-        continueButtonz.setVisible(false);
-        buttonz.setVisible(false); 
-        tutorialText2.setVisible(false);
-    })
-    
-     
+        tutorialText2 = this.add.text(200, 200, "Do you want to engage\n in the tutorial?", {
+            fontFamily: 'Dogica',
+            fontSize: '24px'
+        });
+        tutorialText2.setVisible(true);
+        tutorialText2.setScrollFactor(0, 0);
 
- 
+        tutorialTextBorder = this.add.image(400, 400, 'tutorialTextBorder');
+        tutorialTextBorder.setScrollFactor(0,0);
+
+        var buttonz = this.add.image(255, 600, 'NoButton');
+        buttonz.setScrollFactor(0, 0);
+        buttonz.setInteractive();
+        buttonz.on(pointerOver, () => {buttonz.setFrame(1)});
+        buttonz.on(pointerOut, () => {buttonz.setFrame(0)});
+        buttonz.on(pointerDown, () => {
+            this.cameras.main.fadeOut(1000, 0, 0, 0)
+            this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+                this.scene.start('menu')
+                this.scene.stop();
+            })
+        })
+
+        var continueButtonz = this.add.image(550, 600, 'YesButton');
+        continueButtonz.setScrollFactor(0, 0);
+        continueButtonz.setInteractive();
+        continueButtonz.on(pointerOver, () => {continueButtonz.setFrame(1)});
+        continueButtonz.on(pointerOut, () => {continueButtonz.setFrame(0)});
+        continueButtonz.on(pointerDown, () => {
+            continueButtonz.setVisible(false);
+            buttonz.setVisible(false);
+            tutorialText2.setVisible(false);
+            tutorialTextBorder.setVisible(false); 
+            firstTime = false; 
+        })
 
 
-    const TileTutorialMap = this.make.tilemap({
-        key: 'TileTutorialMap'
-    })
 
-    //Assigns Images To TileMap
-    const TutorialP = TileTutorialMap.addTilesetImage('TutorialP', 'TutorialP', 8, 8, 1, 2);
 
-    MapLayer = TileTutorialMap.createLayer('GrassLayer', TutorialP)
-    CollideLayer = TileTutorialMap.createLayer('CollideLayer', TutorialP)
-    
-    CollideLayer.setCollisionByProperty({                                   //Boundry detection is declared true. 
-        collides: true
-    })
+        const TileTutorialMap = this.make.tilemap({
+            key: 'TileTutorialMap'
+        })
 
-    this.physics.add.collider(player, CollideLayer);
+        //Assigns Images To TileMap
+        const TutorialP = TileTutorialMap.addTilesetImage('TutorialP', 'TutorialP', 8, 8, 1, 2);
+
+        MapLayer = TileTutorialMap.createLayer('GrassLayer', TutorialP)
+
+
 
         const layer = this.add.layer();
-        layer.add([boostpad1, player, buttonz, continueButtonz, tutorialText2, tutorialText]); 
-        
-        
-        
+        layer.add([boostpad1, player,  tutorialTextBorder, buttonz, continueButtonz, tutorialText2, tutorialText]);
+
+
+
         //Defines Keyboard Keys
-        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);      
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -147,69 +153,107 @@ tutorialText2.setScrollFactor(0,0);
         keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         keyESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
-        
+
         //Adds a boostpad to the map
-           //Ads A BoostPad To The Map
-           boostpads = this.add.group();
-           boostpads.add(boostpad1);
-   
-           cars = this.add.group();
-           cars.add(player);
-           
-   
-           //Handles BoostPads and BoostPad Collision
-           this.physics.add.overlap(cars, boostpads, function(user, boostpad) {
-               if (boostpad == boostpad1 && usedBoostPad1 == false) {
-                   usedBoostPad1 = true;
-                   boostpad1.stop("boost");
-                   boostpad1.setFrame(0);
-                   user.body.setMaxSpeed(1000);
-                   user.body.velocity.normalize()
-                       .scale(1000);
-                   setTimeout(function() {
-                       user.body.setMaxSpeed(500);
-                   }, 500);
-               }
-           });
+        //Ads A BoostPad To The Map
+        boostpads = this.add.group();
+        boostpads.add(boostpad1);
+
+        cars = this.add.group();
+        cars.add(player);
+
+
+        //Handles BoostPads and BoostPad Collision
+        this.physics.add.overlap(cars, boostpads, function(user, boostpad) {
+            if (boostpad == boostpad1 && usedBoostPad1 == false) {
+                usedBoostPad1 = true;
+                boostpad1.stop("boost");
+                boostpad1.setFrame(0);
+                user.body.setMaxSpeed(1000);
+                user.body.velocity.normalize()
+                    .scale(1000);
+                setTimeout(function() {
+                    user.body.setMaxSpeed(500);
+                }, 500);
+            }
+        });
     },
 
-update: function(){
-    var input = true; 
-    p1Tile = MapLayer.getTileAtWorldXY(player.x, player.y, true);
-    
-    if (p1Tile.index == 30 && seconds <= 6){
-        console.log(p1Tile.index); 
-        textValue = "This is placeholder text lol"; 
-        tutorialText.setVisible(true); 
-        input = false; 
-        player.setAngularVelocity(0); 
-        player.body.speed = 0; 
-        player.body.drag.x = 5000;
-        player.body.drag.y = 5000;
-       if (player.body.speed > 0){
-           player.body.speed = 0;
-           console.log(player.body.speed);
-       }
-        timerEvent(); 
-        console.log("seconds" + seconds); 
-        
-    }else{
-        if (seconds >= 6 && p1Tile.index == 30){
-        input = true; 
-        
-        console.log("input is restored"); 
+    update: function() {
+        var input = true;
+
+        checkpoint = false;
+        p1Tile = MapLayer.getTileAtWorldXY(player.x, player.y, true);
+
+        if (p1Tile.index == 25 && seconds <= 6) {
+            checkpoint = true;
+            if (checkpoint == true) {
+                count = count + 1;
+                if (count % 420 == 0) {
+                    onCheckpoint = onCheckpoint + 1;
+                    
+                }
+            }
+
+
+            //textValue = "This is placeholder text lol";
+            tutorialText.setVisible(true);
+            input = false;
+            player.setAngularVelocity(0);
+            player.body.moves = false
+            if (onCheckpoint == 0) {
+                tutorialText.setText("Hi");
+                tutorialTextBorder.setVisible(true); 
+            } else if (onCheckpoint == 1) {
+                tutorialText.setText("Yo");
+                tutorialTextBorder.setVisible(true); 
+            } else if (onCheckpoint == 2) {
+                tutorialText.setText("How are you doing");
+                tutorialTextBorder.setVisible(true); 
+            } else if (onCheckpoint == 3) {
+                tutorialText.setText("Stop leaving me on read");
+                tutorialTextBorder.setVisible(true); 
+            } else if (onCheckpoint == 4) {
+                tutorialText.setText("ihy");
+                tutorialTextBorder.setVisible(true); 
+            } else if (onCheckpoint == 5) {
+                tutorialText.setText("we're done");
+                tutorialTextBorder.setVisible(true); 
+            } else if (onCheckpoint == 6) {
+                this.cameras.main.fadeOut(1000, 0, 0, 0)
+                this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+                    this.scene.start('menu')
+                    this.scene.stop();
+                });
+                if (firstTime == false){
+                    tutorialTextBorder.setVisible(false); 
+                }
+            }
+            timerEvent();
+
+
+        } else {
+            if (seconds >= 6 && p1Tile.index == 25) {
+                input = true;
+                player.body.moves = true;
+                tutorialText.setVisible(false);
+            }
+            if (seconds >= 6 && p1Tile.index != 25) {
+                timer = 0;
+                timez = 0;
+                seconds = 0;
+                input = true;
+                tutorialText.setVisible(false);
+            }
+            input = true;
+            player.body.moves = true;
+            tutorialText.setVisible(false);
+            if (firstTime == false) {
+                tutorialTextBorder.setVisible(false);
+            }
         }
-        if (seconds >=3 && p1Tile.index != 30){
-            timer = 0;
-            timez = 0;
-            seconds = 0; 
-            input = true; 
-        }
-        input = true; 
-        tutorialText.setVisible(false); 
-    }
-    
-    
+
+
         if (player.body.speed > 15 && (keyLEFT.isDown) && input == true) {
             player.setAngularVelocity(-150);
         } else if (player.body.speed > 15 && (keyRIGHT.isDown) && input == true) {
@@ -217,7 +261,7 @@ update: function(){
         } else {
             player.setAngularVelocity(0);
         }
-        if (keyUP.isDown && player.body.speed < 516  && input == true) {
+        if (keyUP.isDown && player.body.speed < 516 && input == true) {
             this.physics.velocityFromRotation(player.rotation, 700, player.body.acceleration);
         } else if (player.body.speed > 400 && input == true) {
             this.physics.velocityFromRotation(player.rotation, (player.body.speed - 75), player.body.velocity);
@@ -229,17 +273,17 @@ update: function(){
         }
 
         //in grass, the car is slowed down
-        if (p1Tile.index == 10 || p1Tile.index == 9 || p1Tile.index == 26){
-            
+        if (p1Tile.index == 10 || p1Tile.index == 9 || p1Tile.index == 26) {
+
             if (player.body.speed > 300) {
                 masSpeed = (masSpeed - 7)
             }
             ranOnce = true;
             player.body.setMaxSpeed(masSpeed)
             if (player.body.speed < 310) {
-                if (player.body.speed > 15 && (keyLEFT.isDown )) {
+                if (player.body.speed > 15 && (keyLEFT.isDown)) {
                     player.setAngularVelocity(-50);
-                } else if (player.body.speed > 15 && (keyRIGHT.isDown )) {
+                } else if (player.body.speed > 15 && (keyRIGHT.isDown)) {
                     player.setAngularVelocity(50);
                 } else {
                     player.setAngularVelocity(0);
@@ -261,10 +305,12 @@ update: function(){
             }
         }
 
-        }, 
-});  
 
-function timerEvent(){
+
+    },
+});
+
+function timerEvent() {
     if (1 == 1) {
         while (timer <= 100) {
             timer = timer + 01;
@@ -278,7 +324,5 @@ function timerEvent(){
         timez = 00;
         seconds = seconds + 01;
     }
-    
+
 }
-
-
